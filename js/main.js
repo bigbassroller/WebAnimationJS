111c7ba0f215e983c2650fae72b8c8d5443a5666
Michael Chavez <bigbassroller@gmail.com>
2015-06-25 16:57:26 -0700

    value operators

--- js/main.js
+++ js/main.js
@@ -1,24 +1,8 @@
-
-// Assign a variable to a jQuery element object
-var $div = $("div");
-// Animate the element using Velocity
-$div.velocity({ opacity: 0.5 });
-
-$('.col-md-4').velocity({ width: "500px", opacity: 1 }, { duration: 400, easing: "swing" });
-
-$div.velocity({ opacity: 1 });
-
-$('.col-md-4').velocity({ width: "33.3%", opacity: 1 }, { duration: 400, easing: "swing" });
-
-//Animate with a duration of 1000ms and an easing of "ease-in-out"
-$('.col-md-4').velocity({ top: 500}, 1000);
-$('.col-md-4').velocity({ top: 0}, 1000);
-$('.col-md-4').velocity({ top: 500}, 1000, "ease-in-out");
-$('.col-md-4').velocity({ top: 0}, "ease-in-out");
-$('.col-md-4').velocity({ top: 500}, 1000, function() {alert("complete.")});
-$('.col-md-4').velocity({ top: 0}, "ease-in-out");
-
-$('.col-md-4').velocity({ top: 500 }, { duration: 1000, easing: "swing", delay: 500 });
-
-$('.col-md-4').velocity({ top: 0 }, { duration: 1000, easing: "swing", delay: 1000 });
-
+$('.col-md-4').velocity({ 
+	top: "50px", // No operator. Animate toward 50 as expected.
+	left: "-50", // Negative operator. ANmate toward -50 as expected.
+	width: "+=5rem", // Convert the current width value into its rem equivalent and add 5 more units.
+	height: "-10rem", // Convert the current height value into its rem equivalent and subtract 10 units.
+	paddingLeft: "*=2", // Double the current paddingLeft value.
+	paddingRight: "/=2" // Divide the current paddingRight value into two. 
+	});
