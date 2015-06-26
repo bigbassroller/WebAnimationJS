9b78ddb997c1ea22c67f19478fe4bcbee077b238
Michael Chavez <bigbassroller@gmail.com>
2015-06-25 17:02:19 -0700

    chaining

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-$('.col-md-4').velocity({ 
-	top: "50px", // No operator. Animate toward 50 as expected.
-	left: "-50", // Negative operator. ANmate toward -50 as expected.
-	width: "+=5rem", // Convert the current width value into its rem equivalent and add 5 more units.
-	height: "-10rem", // Convert the current height value into its rem equivalent and subtract 10 units.
-	paddingLeft: "*=2", // Double the current paddingLeft value.
-	paddingRight: "/=2" // Divide the current paddingRight value into two. 
-	});
+$('.col-md-4').velocity({ width: "100px", height: "100px" }).velocity({ top: "50px" }).velocity({ width: "33.33%", height: "100%" }).velocity({ top: "0px" })
