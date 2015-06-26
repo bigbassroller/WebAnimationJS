170d4f1dd5bc368e96fc5bc192518c7f153ee402
Michael Chavez <bigbassroller@gmail.com>
2015-06-25 23:07:02 -0700

    using velocity without jquery

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
-$(".col-md-4").velocity({
-	translateZ: "200px",
-	rotateZ: "45deg"
-});
-$(".col-md-4").velocity({
-	translateY: "-200px",
-	rotateY: "60deg"
-});
-$(".col-md-4").velocity({
-	translateX: "400px",
-	rotateX: "50deg"
-});
-$(".col-md-4").velocity({
-	translateZ: "200px",
-	rotateZ: "90deg"
-});
-$(".col-md-4").velocity({
-	translateY: "200px",
-	rotateY: "45deg"
-});
-$(".col-md-4").velocity({
-	translateX: "-400px",
-	scaleY: ".5",
-	rotateY: "320deg"
-});
+// Velocity(document.getElementById("section1"), { opacity: 0.5 }, 1000);
+var divs = document.querySelectorAll("div");
+
+Velocity(divs, { opacity: 0.5 }, 1000);
+Velocity(divs, { opacity: 1 }, 1000);
