3fbfb60b179511030ec68b163bbd401710ed2ef4
Michael Chavez <bigbassroller@gmail.com>
2015-06-25 22:42:08 -0700

    colors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,2 @@
-$("#section3").velocity("scroll", { 
-  axis: "x"
-});
+$(".col-md-4").velocity({ backgroundColor: "#000000", backgroundColorAlpha: 0.5, colorRed: 125 });
+$(".col-md-4").velocity( "reverse", { duration: 2000  });
