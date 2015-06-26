1e073c87038855915d2675bf714b8bc63fe145ee
Michael Chavez <bigbassroller@gmail.com>
2015-06-25 22:54:08 -0700

    transforms

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
-$(".col-md-4").velocity({ backgroundColor: "#000000", backgroundColorAlpha: 0.5, colorRed: 125 });
-$(".col-md-4").velocity( "reverse", { duration: 2000  });
+$(".col-md-4").velocity({
+	translateZ: "200px",
+	rotateZ: "45deg"
+});
+$(".col-md-4").velocity({
+	translateY: "-200px",
+	rotateY: "60deg"
+});
+$(".col-md-4").velocity({
+	translateX: "400px",
+	rotateX: "50deg"
+});
+$(".col-md-4").velocity({
+	translateZ: "200px",
+	rotateZ: "90deg"
+});
+$(".col-md-4").velocity({
+	translateY: "200px",
+	rotateY: "45deg"
+});
+$(".col-md-4").velocity({
+	translateX: "-400px",
+	scaleY: ".5",
+	rotateY: "320deg"
+});
