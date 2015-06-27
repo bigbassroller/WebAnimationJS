d18753f1105f0d0c26368443866c68dcce4d151d
Michael Chavez <bigbassroller@gmail.com>
2015-06-26 23:57:36 -0700

    register effect

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-$.Velocity.RunSequence([
-	{ elements: $(".foo"), properties: { translateX: 100 }, options: { duration: 1000 }},
-	{ elements: $(".bar"), properties: { translateX: 200 }, options: { duration: 1000, sequenceQueue: false }},
-	{ elements: $(".ski"), properties: { translateX: 300 }, options: { duration: 1000 }}
-]);
+// $.Velocity.RunSequence([
+// 	{ elements: $(".foo"), properties: { translateX: 100 }, options: { duration: 1000 }},
+// 	{ elements: $(".bar"), properties: { translateX: 200 }, options: { duration: 1000, sequenceQueue: false }},
+// 	{ elements: $(".ski"), properties: { translateX: 300 }, options: { duration: 1000 }}
+// ]);
+$(".col-md-4").velocity("shadowIn").velocity("shadowOut");
--- js/style.js
+++ js/style.js
@@ -12,3 +12,20 @@ var fadeIn = {
 		easing: "linear"
 	}
 };
+
+$.Velocity
+	.RegisterEffect("shadowIn", {
+		defaultDuration: 1000,
+		calls: [
+			[ { opacity: 1, scale : 1 }, 0.4 ],
+			[ { boxShadowBlur: 50 }, 0.6 ]
+		]
+	})
+	.RegisterEffect("shadowOut", {
+		defaultDuration: 800,
+		calls: [
+			// we reverse the order to mirror th "In" direction
+			[ { boxShadowBlur: 50 }, 0.2 ],
+			[ { opacity: 0, scale: 0 }, 0.8 ]
+		]
+	});
