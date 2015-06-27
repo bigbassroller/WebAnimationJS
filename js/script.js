aee65fa7acb650123b2cacac11f0a3063c8542ba
Michael Chavez <bigbassroller@gmail.com>
2015-06-26 18:19:29 -0700

    Simplified

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-var loadingSequence = [
+$.Velocity.RunSequence([
 	{ e: $(".foo"), p: { translateX: 100, opacity: 1 }, o: fadeIn.oSlow},
 	{ e: $(".bar"), p: { translateX: 200, opacity: 1 }, o: fadeIn.oSlow},
 	{ e: $(".ski"), p: { translateX: 300, opacity: 1 }, o: fadeIn.oSlow}
 
-];
-
-$.Velocity.RunSequence(loadingSequence);
+]);
